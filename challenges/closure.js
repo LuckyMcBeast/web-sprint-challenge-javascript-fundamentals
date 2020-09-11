45f63f7dbc5be7d12e80e14f71bda60fcb87e245
M. Cullen McClellan <cullenmcclellan@gmail.com>
2020-09-11 19:54:58 -0400

    Array-Callbacks and Closure Complete

--- challenges/closure.js
+++ challenges/closure.js
@@ -17,8 +17,20 @@ function myFunction() {
 myFunction();
 
 // Explanation: 
-
+// Because internal is defined within the function and only exist while the function is called.
 
 /* Task 2: Counter */
 
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+
+function summation(num){
+  let counter = num;
+  let value = 0;
+  while(counter > 0){
+    value += counter;
+    counter--;
+  }
+  return value;
+}
+
+console.log(summation(4));
